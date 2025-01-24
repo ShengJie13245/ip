2cb4a2e3c1c1b01c1bc53cfe6fb96475c86d9101
ShengJie <ksj3191z@gmail.com>
2025-01-24 22:37:22 +0800

    Add echo function

--- src/main/java/Sss.java
+++ src/main/java/Sss.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Sss {
     public static void main(String[] args) {
         System.out.println("Hello from Sss\n");
         System.out.println("Nice to meet you");
-        System.out.println("Goodbye, see you next time");
+
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Command");
+            System.out.println("-------------------------");
+
+
+            String command = input.nextLine();
+            if(command.equalsIgnoreCase("Bye")){
+                System.out.println("Goodbye, see you next time");
+                break;
+            } else {
+                System.out.println(command);
+                System.out.println("-------------------------");
+            }
+        }
     }
 }
