06064d70706e18c36b7aa6c1cc5f94342415b06b
ShengJie <ksj3191z@gmail.com>
2025-01-24 10:00:23 +0800

    Change name, add greetings, add exit

---
+++ src/main/java/Sss.java
@@ -0,0 +1,7 @@
+public class Sss {
+    public static void main(String[] args) {
+        System.out.println("Hello from Sss\n");
+        System.out.println("Nice to meet you");
+        System.out.println("Goodbye, see you next time");
+    }
+}
