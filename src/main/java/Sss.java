c3e64acf62a8905732be840683b420a80567d7d9
ShengJie <ksj3191z@gmail.com>
2025-01-24 23:08:18 +0800

    Add ability to store and display text

--- src/main/java/Sss.java
+++ src/main/java/Sss.java
@@ -2,23 +2,39 @@ import java.util.Scanner;
 
 public class Sss {
     public static void main(String[] args) {
-        System.out.println("Hello from Sss\n");
+        System.out.println("Hello from Sss");
         System.out.println("Nice to meet you");
+        String[] tasks = new String[100];
+        int counter = 0;
+        boolean continueProgram = true ;
 
-        while(true){
+        while(continueProgram){
             Scanner input = new Scanner(System.in);
-            System.out.println("Enter Command");
             System.out.println("-------------------------");
-
-
+            System.out.println("Enter Command\n");
+            
             String command = input.nextLine();
-            if(command.equalsIgnoreCase("Bye")){
+            switch(command){
+            case ("bye"):
                 System.out.println("Goodbye, see you next time");
+                continueProgram = false;
                 break;
-            } else {
-                System.out.println(command);
+            
+            case ("list"):
+                for (int i = 0; i<counter; i++){
+                    System.out.println((i+1) + "." + tasks[i]);
+                }
                 System.out.println("-------------------------");
-            }
+                break;
+
+            default:   
+                tasks[counter] = command;
+                System.out.println("added: " + command);
+                counter += 1;
+                System.out.println("-------------------------");
+
+            } 
         }
     }
 }
+
