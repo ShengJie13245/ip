bd4dff45b5fabee7e2cdc2fb81ab90540d9dfa50
ShengJie <ksj3191z@gmail.com>
2025-01-27 20:30:42 +0800

    Add mark as done

--- src/main/java/Sss.java
+++ src/main/java/Sss.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Sss {
     public static void main(String[] args) {
         System.out.println("Hello from Sss");
         System.out.println("Nice to meet you");
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int counter = 0;
         boolean continueProgram = true ;
+        int index;
 
         while(continueProgram){
             Scanner input = new Scanner(System.in);
             System.out.println("-------------------------");
             System.out.println("Enter Command\n");
+
+            String inputString = input.nextLine();
+            String[] inputArray = inputString.split(" ");
+            String command = inputArray[0];
             
-            String command = input.nextLine();
             switch(command){
             case ("bye"):
                 System.out.println("Goodbye, see you next time");
@@ -26,10 +31,25 @@ public class Sss {
                 }
                 System.out.println("-------------------------");
                 break;
+            
+            case ("mark"):
+                index = Integer.parseInt(inputArray[1]);
+                tasks[index].setDoneStatus(true);
+                System.out.println("Marked these:");
+                System.out.println(tasks[index].getStatus());
+                break;
 
-            default:   
-                tasks[counter] = command;
-                System.out.println("added: " + command);
+            case ("unmark"):
+                index = Integer.parseInt(inputArray[1]);
+                tasks[index].setDoneStatus(false);
+                System.out.println("Unmarked these:");
+                System.out.println(tasks[index]);
+                break;
+
+            default:
+                Task newTask = new Task(inputString);
+                tasks[counter] = newTask;
+                System.out.println("added: " + inputString);
                 counter += 1;
                 System.out.println("-------------------------");
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String taskDescription;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.taskDescription = description;
+        this.isDone = false;
+    }
+
+    public String getStatus(){
+        return (isDone ? "[X] " + taskDescription : "[ ] " + taskDescription);
+    }
+
+    public void setDoneStatus(boolean doneStatus){
+        this.isDone = doneStatus;
+    }
+
+    @Override public String toString() {
+        return getStatus();
+    }
+}
